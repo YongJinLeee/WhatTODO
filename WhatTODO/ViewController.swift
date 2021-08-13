c4a66cc3e9ad9814233216d55620f293c65287fa
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-14 02:37:06 +0900

    initial Commit : 과업지시 작성

--- WhatTODO/ViewController.swift
+++ WhatTODO/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // Task 추가 제거, Tab Bar Controller
+    // Data 관리 - NSCoding, Property List, Serialization, Core Data, Realm 등등
+    // 적고 덜 복잡한 데이터 관리 -> NSCoding, Peroperty List
+    // 많고 복잡한 데이터 관리 -> Core data, Realm
+    
+    // 최근 사용 경향 Cadable(Swift4에 추가, NSCoding보다는 덜 복잡하고 더 직관적이며, JSON과 유사한 프로토콜 제공)
 
 
 }
