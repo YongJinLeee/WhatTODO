9a86f28414c96a5f6d1609da9b85c74799532a7f
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-25 23:24:24 +0900

    Feat : Todo List의 각 object(View) 동작 관련 함수 추가
    1. WhatTodoListCell class
    
    - 취소선 표시 여부 확인 함수 showCancelLine. task의 완료정보를 받아와 bool값이 true일 경우 취소선 표시
    - 재시작, 셀 재사용 등으로 Storyboard 다시 호출할 때 View 상태 초기화할 reset 함수 추가
    - check 버튼 토글 상태에 따라 View에 표시될 내용 및 실제 상황에 맞는 data 업데이트 - 가 되도록 핸들러에 로드
    - delete 버튼도 마찬가지로 버튼이 눌렸다는 것을 핸들러에 담고, 실제 구현은 ViewModel에서 이루어지도록 구현
    
    2. main.storyboard
    
    - CheckIcon Button state: selected 설정 (checkMark icon)

--- WhatTODO/WhatTodoListCell.swift
+++ WhatTODO/WhatTodoListCell.swift
@@ -2,11 +2,12 @@
 //  WhatTodoListCell.swift
 //  WhatTODO
 //
-//  Created by LeeYongJin on 2021/08/19.
+//  Created by LeeYongJin
 //
 
 import UIKit
 
+// MVVM : View
 class WhatTodoListCell: UICollectionViewCell {
 
     @IBOutlet weak var checkIcon: UIButton!
@@ -23,11 +24,12 @@ class WhatTodoListCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        reset()
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        reset()
     }
     
     func updateUI(task: TodoData) {
@@ -36,7 +38,37 @@ class WhatTodoListCell: UICollectionViewCell {
         descriptionsLabel.alpha = task.isDone ? 0.2 : 1  // 투명도
         deleteButton.isHidden = task.isDone == false
         
+        showCancelLine(task.isDone) // 취소선 긋기 여부
+    }
+    // true인 경우 취소선
+    private func showCancelLine(_ show: Bool) {
+        if show {
+            cancelLineThroughWidth.constant = descriptionsLabel.bounds.width
+        } else {
+            cancelLineThroughWidth.constant = 0
+        }
     }
     
+    // app 종료, 재시작 등으로 UI를 재설정 및 재배열하는 함수. 일종의 초기화
+    func reset() {
+        descriptionsLabel.alpha = 1 //투명도 0
+        deleteButton.isHidden = true
+        showCancelLine(false)
+    }
     
+    func checkBtnToggle(_ sender: Any) {
+        checkIcon.isSelected = !checkIcon.isSelected
+        //view 조정
+        let isDone = checkIcon.isSelected
+        showCancelLine(isDone)
+        descriptionsLabel.alpha = isDone ? 0.2 : 1
+        deleteButton.isHidden = !isDone // 반대 관계 (true: 숨김 / false: 보임)
+        
+        // 실제 data 변경할 핸들러
+        doneBtnTapHandler?(isDone)
+    }
+    func deleteBtnTapped(_ sender: Any) {
+        // 탭이 되었다는 사실만 핸들러에 담고, 실제 연산은 외부에서 이루어지도록 구현
+        delBtnTapHandler?()
+    }
 }
