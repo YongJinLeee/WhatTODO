03ad2c05749b2f88f752f010e286e24ff10ec096
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-27 04:06:18 +0900

    Feat : 파일 관리 기능(remove, directory clear) 추가

--- WhatTODO/Storage.swift
+++ WhatTODO/Storage.swift
@@ -47,17 +47,19 @@ public class Storage {
         encoder.outputFormatting = .prettyPrinted
         
         do {
-            let data = try? encoder.encode(object)
+            let data = try encoder.encode(object)
             // data는 이때 Data 타입으로 캐스팅됨
             // 인코딩을 통해 JSON 파일을 만들고, 해당 url 경로에 이미 파일이 존재하면 해당 파일을 지우고
             if FileManager.default.fileExists(atPath: url.path) {
-                try? FileManager.default.removeItem(at: url)
+                try FileManager.default.removeItem(at: url)
             }
             FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil) // 검사 후 파일 생성
         } catch let error {
             print("저장에 실패했습니다. error: \(error.localizedDescription)")
         }
     }
+    
+    // throws -> try에 do-catch 예외처리가 되면 굳이 try 뒤에 ?로 옵셔널하게 사용하지 않아도 된다.
     
     // Decoding JSON
     // Data 타입형태(codable decode 가능한)로 파일 읽음
@@ -73,13 +75,38 @@ public class Storage {
         do {
             // decodable protocol을 준수하는 T, T를 변수타입으로 받는 type
             // 파일을 data 로 받음
-            let model = try? decoder.decode(type, from: data)
+            let model = try decoder.decode(type, from: data)
             return model
         } catch let error {
             print("파일 파싱에 실패했습니다(Failed of decode). error: \(error.localizedDescription)")
             return nil
         }
     }
-    
+    // JSON Data파일 삭제
+    static func remove(_ fileName: String, from directory: Directory) {
+        let url =  directory.url.appendingPathComponent(fileName, isDirectory: false)
+        // 오류 발생 가능성 때문에 옵셔널 바인딩
+        guard FileManager.default.fileExists(atPath: url.path) else { return }
+        
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let error {
+            print("파일 삭제에 실패했습니다(Failed of remove). error: \(error.localizedDescription)")
+        }
+    }
+    //  사용후 경로내 디렉토리 초기화
+    static func clear(_ directory: Directory) {
+        let url = directory.url
+        
+        do {
+            let contnets = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+            for content in contnets {
+                try FileManager.default.removeItem(at: content)
+            }
+        } catch {
+            print("디렉토리 초기화에 실패했습니다(Failed of directory clear). error: \(error.localizedDescription)")
+        }
+        
+    }
     
 }
