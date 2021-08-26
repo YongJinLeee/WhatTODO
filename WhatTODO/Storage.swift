f20cc01002bfd5e78396dde161cb3a161b6be1db
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-27 03:50:58 +0900

    Feat : codable protocol을 준수한 파일매니징 method 구현
    - Foundation 모듈의 interface class인 NSFileManager를 이용해 Storage class 안에 Codable protocol 을 준수하는 encoding, decoding method 구현
    - Encode, decode 모두 처리 과정에서 오류가 발생할 여지가 있어 프로토콜 상 throws로 오류를 던지기 때문에 try do-catch로 예외처리

--- WhatTODO/Storage.swift
+++ WhatTODO/Storage.swift
@@ -10,9 +10,76 @@ public class Storage {
     // business Logic 캡슐화
     private init() { }
     
+    // JSON은 [Key : value] 와 같은 dictionary
+    // Codable protocol 준수
     enum Directory {
         case documents
         case caches
+        
+        var url: URL {
+            let path: FileManager.SearchPathDirectory
+            
+            switch self {
+            case .documents:
+                path = .documentDirectory
+            case .caches:
+            path = .cachesDirectory
+                
+            }
+            // 사용자 지정 디렉토리로 반환
+            return FileManager.default.urls(for: path, in: .userDomainMask).first!
+        }
     }
-
+    
+    // zediios님의 관련 글 참조
+    // https://zeddios.tistory.com/373
+    // JSON Encoding method
+    static func store<T: Encodable>(_ object: T, to directory: Directory, as fileName: String) {
+        let url = directory.url.appendingPathComponent(fileName, isDirectory: false)
+        // Test
+        // 파일 저장위치 console 확인용
+        print("저장된 파일 디렉토리 주소 : \(url)")
+        
+        let encoder = JSONEncoder()
+        // encoding 결과물 출력 포멧
+        // .prettyPrinted -> JSON 포맷으로 표기
+        // .sortedKey -> Key값기준 오름차순 정렬
+        encoder.outputFormatting = .prettyPrinted
+        
+        do {
+            let data = try? encoder.encode(object)
+            // data는 이때 Data 타입으로 캐스팅됨
+            // 인코딩을 통해 JSON 파일을 만들고, 해당 url 경로에 이미 파일이 존재하면 해당 파일을 지우고
+            if FileManager.default.fileExists(atPath: url.path) {
+                try? FileManager.default.removeItem(at: url)
+            }
+            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil) // 검사 후 파일 생성
+        } catch let error {
+            print("저장에 실패했습니다. error: \(error.localizedDescription)")
+        }
+    }
+    
+    // Decoding JSON
+    // Data 타입형태(codable decode 가능한)로 파일 읽음
+    // file
+    static func retrive<T: Decodable>(_ fileName: String, from directory: Directory, as type: T.Type) -> T? {
+        
+        let url = directory.url.appendingPathComponent(fileName, isDirectory: false)
+        guard FileManager.default.fileExists(atPath: url.path) else { return nil } // 해당경로에 파일이 있다면 decoding 진행하고 아니면 nil 반환
+        guard let data = FileManager.default.contents(atPath: url.path) else { return nil } // type casting (Data)
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            // decodable protocol을 준수하는 T, T를 변수타입으로 받는 type
+            // 파일을 data 로 받음
+            let model = try? decoder.decode(type, from: data)
+            return model
+        } catch let error {
+            print("파일 파싱에 실패했습니다(Failed of decode). error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    
 }
